54a1f64c68be3624eeeada78b9d5fcd0bcd3e1ee
Filipe Valeriano <filipevaleriano85@gmail.com>
2025-03-09 04:09:34 +0000

    COMMIT_BatalhaNaval-Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,47 +1,69 @@
 #include <stdio.h>
 
+#define linhas 10 // para definir quantas linhas tera a matriz
+#define colunas 10 // para definir quantas colunas tera a matriz
 
-int main(){
+// Função para inicializar o tabuleiro usando a recursividade
+void inicializarTabuleiro(int iniciar[linhas][colunas]) {
 
-    char letras[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; // matriz para definir um arrays de letras de A a J no tabuleiro.
-
-    printf("###### TABULEIRO BATALHA NAVAL ######\n");
-
-    printf(" "); // espaço para manter o tabuleiro de forma alinhada.
-    for(int i = 0; i < 10; i++){ // loop para impressão das letras de forma horizontal no tabuleiro.
-        printf(" %c", letras[i]);
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            iniciar[i][j] = 0; 
     }
-    int tabuleiro[10][10]; // Tabuleiro 10x10
-    int i, j; 
-    for (i = 0; i < 10; i++) { // for externo usando o tabuleiro 10x10 para impressão dos numeros de 0 a 9 de forma vertical
+}
+}
 
-        printf("\n%d ", i); // usei o printf do for externo antes da execursão do for interno, para deixar os numeros de 0 a 9 de forma mais interativa com o tabuleiro.
+// Função para desenhar um triângulo no tabuleiro usando a recursividade
+void criarDesenho(int iniciar[linhas][colunas], int x, int y, int tamanho){
 
-        for (j = 0; j < 10; j++) { // for interno usado para criar o 'mar' usando a matriz tabuleiro 
-            tabuleiro[i][j] = 0; // definir duas variaveis 'i' e 'j' antes do for, para atribuir ao tabuleiro e atribuindo o valor = 0. Para criar o mar de zero na matriz tabuleiro.
+    for (int i = 0; i < tamanho; i++) { // esse for externo determina a altura do triângulo
+        for (int j = -i; j <= i; j++) { // esse for interno determina a largura do triângulo
+            int pontX = x + i; // de acordo com o numero atribuido ao x ele ira somar com o i
+            int pontY = y + j; // de acordo com o numero atribuido ao y ele ira somar com j
+            if (pontX >= 0 && pontX < linhas && pontY >= 0 && pontY < colunas) { // if usado para criar a logica do programa.
+                iniciar[pontX][pontY] = 5; // função para pintar o triângulo 
+            }
+      
 
-            // Navio na forma Diagonal direita.
-            tabuleiro[1][8] = 3; // a linha 9 contando de 0 a 8 do numero 1 vai receber o numero 3
-            tabuleiro[2][7] = 3; // a linha 8 contando de 0 a 7 do numero 2 vai receber o numero 3
-            tabuleiro[3][6] = 3; // a linha 7 contando de 0 a 6 do numero 3 vai receber o numero 3
-            // Navio na forma Diagonal esquerda.
-            tabuleiro[6][0] = 3; // a linha 1 contando de 0 a 1 do numero 6 vai receber o numero 3
-            tabuleiro[7][1] = 3; // a linha 2 contando de 0 a 2 do numero 7 vai receber o numero 3
-            tabuleiro[8][2] = 3; // a linha 3 contando de 0 a 3 do numero 8 vai receber o numero 3
-            // Navio na forma Horizontal.
-            tabuleiro[5][8] = 3; // a linha 9 contando de 0 a 8 do numero 5 vai receber o numero 3
-            tabuleiro[5][6] = 3; // a linha 7 contando de 0 a 6 do numero 5 vai receber o numero 3
-            tabuleiro[5][7] = 3; // a linha 8 contando de 0 a 7 do numero 5 vai receber o numero 3
-            // Navio na forma Vertical.
-            tabuleiro[4][1] = 3; // a linha 1 contando de 0 a 1 do numero 4 vai receber o numero 3
-            tabuleiro[3][1] = 3; // a linha 1 contando de 0 a 1 do numero 3 vai receber o numero 3
-            tabuleiro[2][1] = 3; // a linha 1 contando de 0 a 1 do numero 2 vai receber o numero 3
-
-            printf("%d ", tabuleiro[i][j]); // exibição do tabuleiro, com as alteraçães predefinidas no codigo.
         }
     }
 
 
-    return 0;
 }
+
+// Função para imprimir o tabuleiro usando a recursividade
+void iniciarTabuleiro(int iniciar[linhas][colunas]){
+int number[10]={0,1,2,3,4,5,6,7,8,9};
+printf(" ");
+for(int i = 0; i < 10; i++){ // for usado para imprimir de 0 a 9 de forma horizontal na parte de cima do tabuleiro.
+    printf(" %d", number[i]); 
+}
+    for (int i = 0; i < linhas; i++) { // for externo usado para imprimir de 0 a 9 de forma vertical do lado esquerdo do tabuleiro.
+    printf("\n%d ", i); 
+
+        for (int j = 0; j < colunas; j++) { // for interno usado para imprimir uma matriz bidimensiona 10x10.
+        printf("%d ", iniciar[i][j]); 
+        }
+ 
+    }
+}
+
+ 
+int main() {
+    int tabuleiro[linhas][colunas];
+    
+    // função recursiva para ativar o tabuleiro
+    inicializarTabuleiro(tabuleiro);
+    
+    // função recursiva para ativar o desenho no tabuleiro
+    criarDesenho(tabuleiro, 1, 2, 3);
+    
+    // função recursiva para imprimir o tabuleiro
+    iniciarTabuleiro(tabuleiro);
+
+    
+    
+
+        return 0;
+    }
 
